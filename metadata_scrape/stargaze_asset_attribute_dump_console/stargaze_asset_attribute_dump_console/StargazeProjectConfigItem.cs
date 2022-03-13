b8d6bf19ca187350bc07404b684d39c90f8bc77e
arizonawayfarer <three.sixteen@gmail.com>
2022-03-13 09:49:34 -0700

    Adding C# console scraper and results of the scrape for Stargaze Punks

---
+++ metadata_scrape/stargaze_asset_attribute_dump_console/stargaze_asset_attribute_dump_console/StargazeProjectConfigItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stargaze_asset_attribute_dump_console
+{
+    internal class StargazeProjectConfigItem
+    {
+        public string Name { get; set; }
+        public string TokenMetaDataAddress { get; set; }
+
+        public int MaxMint { get; set; }
+    }
+}
